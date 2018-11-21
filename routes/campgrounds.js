318627c48fcea151bc57e2db3f23db423162d565
Leo Santillan <lcskode@gmail.com>
2018-11-21 19:11:42 +0800

    Added edit and update routes in campgrounds route, added form for editing campgrounds, added edit button in show campground.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -68,7 +68,8 @@ router.get("/:id/edit", function(req, res){
   // find campground by ID
   Campground.findById(req.params.id, function(err, foundCampground){
     if(err){
-      console.log("error at /campgrounds/:id/edit route: " + err);
+      // console.log("error at /campgrounds/:id/edit route: " + err);
+      res.redirect("/campgrounds");
     } else {
       res.render("campgrounds/edit", {campground: foundCampground});
     } 
@@ -76,7 +77,17 @@ router.get("/:id/edit", function(req, res){
 });
 
 // UPDATE Campground ROUTE
-
+router.put("/:id", function(req, res){
+  // find and update the correct campground
+  // Campground.findByIdAndUpdate(ID, DATA, CALLBACK)
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+    if(err){
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
 
 // MIDDLEWARE
 // add isLoggedIn middleware 
